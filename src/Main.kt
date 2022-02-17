3b18e341ac44175737000e573a033d9306ebce5b
WaceraMercyThird <waceraaamercy@gmail.com>
2022-02-17 20:58:57 +0300

    myFirstProjct

---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+
+fun main() {
+
+    var names = "Student"
+    var age = "Number"
+    var pnumber ="PhoneNumber"
+    var weight = 45.4
+    val isKenyan:Boolean = true
+    var isNotKenyan:Boolean = false
+
+
+//    First student record...
+    println(names)
+    println(age)
+    println(pnumber)
+    println(weight)
+    println(isKenyan)
+    println(isNotKenyan)
+
+
+
+
+
+
+}
